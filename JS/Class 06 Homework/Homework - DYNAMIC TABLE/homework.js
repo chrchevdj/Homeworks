32468b378c0adff464381b0485d4cea6f11cc1eb
chrchevdj <118832611+chrchevdj@users.noreply.github.com>
2023-01-10 18:52:37 +0200

    Add files via upload

--- JS/Class 06 Homework/Homework - DYNAMIC TABLE/homework.js
+++ JS/Class 06 Homework/Homework - DYNAMIC TABLE/homework.js
@@ -7,33 +7,34 @@ User should input the number of Colums and Rows
 In every table cell print which row and column it is (ex. Row-3 Column-1)
 Don't forget to use google! :)
 */
-
 function generateTable() {
- 
+  // creates a <table> element and a <tbody> element
   let tbl = document.createElement("table");
   let tblBody = document.createElement("tbody");
 
-
+  // creating all cells
   for (let i = 1; i < 4; i++) {
-
+    // creates a table row
     let row = document.createElement("tr");
 
     for (let j = 0; j < 3; j++) {
-      
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
       let cell = document.createElement("td");
       let cellText = document.createTextNode(`Row - ${i}, column - ${j}`);
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
 
-
+    // add the row to the end of the table body
     tblBody.appendChild(row);
   }
 
-
+  // put the <tbody> in the <table>
   tbl.appendChild(tblBody);
-
+  // appends <table> into <body>
   document.body.appendChild(tbl);
-  
+  // sets the border attribute of tbl to '2'
   tbl.setAttribute("border", "2");
 }
